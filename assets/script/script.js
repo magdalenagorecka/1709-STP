bab7bd586223f6c9675f7f39da3fa68f66d27372
Magdalena Górecka <goorecka.magdalena@gmail.com>
2017-09-23 20:01:52 +0200

    VG RSS changes
    Retrieving data from xml, sorting them descending by default, pushing
    to array.
    Sorting button reversing array.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,49 +1,34 @@
 $(document).ready(function() {
-  let urlVin='http://vinguiden-webapp-develop.seals.schibsted.pl/api/product?ids=4';
-
-  function getVin(val){
-    $.ajax({
-      url: urlVin,
-      dataType: "jsonp",
-      success: function(data){
-        $("#vin").html("<p>dupa</p>");
-      }
-    });
-  }
-  getVin();
-
-
-
 
 let rssUrl = "http://www.vg.no/rss/feed/",
-    $rssItem = $(".rssItem");
+    $rssItem = $(".rssItem"),
+    sortArr =[],
+    $sort = $("#sort");
 //using xml parsing funcionality of jquery
 $.get(rssUrl, function(data){
 
-  var sortArr =[];
-
-  var xmlData = $(data).find("item");
+  let xmlData = $(data).find("item");
+//sort desc by default
   xmlData.sort(function(a,b){
     a = $(a).find("pubDate").text();
     b = $(b).find("pubDate").text();
     return(b.localeCompare(a));
   });
-  
+//make an array
   xmlData.each(function(){
     let item={
         title: $(this).find("title").text(),
         pubDate: $(this).find("pubDate").text()
       }
-
-    $rssItem.append('<div class="rss"><h3>' + item.title + '</h3><p class="pubDate">' + item.pubDate + '</p></div>');
-
+    sortArr.push(item);
   });
+  console.log(sortArr);
 });
 
 
-
-
-
-
-
+//sorting button
+  $sort.click(function(){
+    sortArr.reverse();
+    console.log(sortArr);
+  });
 }) //document ready
