d02a448ea2a08746b47e5a385521c437bc291e9f
Magdalena Górecka <goorecka.magdalena@gmail.com>
2017-09-24 11:08:14 +0200

    VIN single select modification
    Retrieving data from single select.

--- assets/script/vin.js
+++ assets/script/vin.js
@@ -2,14 +2,11 @@
 
 $(document).ready(function() {
 //1. wybierz z menu rozwijanego
+let vinURL = "http://vinguiden-webapp-develop.seals.schibsted.pl/api/product?ids=1";
+$('body').on('load',getDefault());
 
-let e = document.getElementById('pages'),
-    page = e.options[e.selectedIndex].value,
-    vinURL = "http://vinguiden-webapp-develop.seals.schibsted.pl/api/product?ids="+page;
-
-
+function getDefault(){
   $.getJSON(vinURL, function(data){
-
       var html="";
       data.forEach(function(val){
 
@@ -21,8 +18,8 @@ let e = document.getElementById('pages'),
         html += "<div class='vinItem__info'>"
         html += "<p>" + val.country + " / " + val.region + "</p>";
         html += "<p><h3>" + val.name + "</h3></p>";
-        html += "<p>" + val.productionYear + " | " + val.product + " | " + val.grapes + "</p>";
-        html += "<p class='vinItem__rate'>Rate: " + val.rate + "</p>";
+        html += "<p>" + val.product + " | " + val.grapes + " | " + val.productionYear + "</p>";
+        html += "<p class='vinItem__rate'> <i class='material-icons'>star_rate</i>Rate: " + val.rate + "</p>";
         html += "<p>Sold Bottles: " + val.soldBottles + "</p>";
         html += "<p><a href='" + val.productUrl + "'>Product's Link</a></p>";
         html += "<p>Availability: " + val.availability + "  Ecological: " + val.ecological + "  Packaging: " + val.packaging + "</p>";
@@ -34,5 +31,27 @@ let e = document.getElementById('pages'),
       $('#vin').html(html);
 
     }, "json");
+  }
+
+
+$("#pages").on('change',function(){
+    page = $(this).find(":selected").val();
+    vinURL = "http://vinguiden-webapp-develop.seals.schibsted.pl/api/product?ids="+page;
+    getDefault();
+});
+
+//***** Menu buttons **********//
+  $('.sort__name').on('click', function(){
+    alert("dupa");
+  });
+
+  $('.sort__price').on('click', function(){
+    alert("dupa2");
+  });
+
+  $('.filter__country').on('click', function(){
+    alert("dupa");
+  });
+
 
 });//ready
