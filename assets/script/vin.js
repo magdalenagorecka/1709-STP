e51c866ff392176e232ecd2ea0dc723c2f5fde9e
Magdalena Górecka <goorecka.magdalena@gmail.com>
2017-09-26 18:34:45 +0200

    VIN IE10 ready
    Adjustments of js (exclude es6) to work with IE10

--- assets/script/vin.js
+++ assets/script/vin.js
@@ -1,13 +1,12 @@
-$(document).ready(function() {
-
-  let vinURL = "http://vinguiden-webapp-develop.seals.schibsted.pl/api/product?ids=1",
-  $pages = $("#pages"),
-  $vin = $('#vin'),
-  countryArray,
-  jsonData,
-  i,
-  jsonDataFiltered,
-  sortOrder = 'asc';
+//$(document).ready(function() {
+  var vinURL = "http://vinguiden-webapp-develop.seals.schibsted.pl/api/product?ids=1";
+  var $pages = $("#pages");
+  var $vin = $('#vin');
+  var countryArray;
+  var jsonData;
+  var i;
+  var jsonDataFiltered;
+  var sortOrder = 'asc';
 
   $('body').on('load', getDefault());
 
@@ -30,86 +29,78 @@ $(document).ready(function() {
         //setting icon for wine type
         val.product === "Rött vin" ? cat = 1 : val.product === "Vitt vin" ? cat = 2 : val.product === "Rosévin" ? cat = 3 : cat = 0;
 
-          html += "<div class='vinItem'>";
-          html += "<div class='vinItem__photo'>";
-          html += "<img src='" + logo + "'>";
-          html += "<p class='vinItem__id'>" + val.id + "</p>";
-          html += "</div>";
-          html += "<div class='vinItem__info'>";
-          html += "<p class='vinItem__country'>" + val.country + " | " + val.region + "</p>";
-          html += "<p><h3>" + val.name + "</h3></p>";
-          html += "<p class='vinItem__category'><span class='cat-" + cat + "'>"+val.product+ "</span> | " + val.grapes + " | " + val.productionYear + "</p>";
-          html += "<p class='vinItem__rate'><span class='star-" + star +"'></span>Rate: " + Math.floor(val.rate*100)/100 + "</p>";
-          html += "<div class='vinItem__small'><p >Sold Bottles: " + val.soldBottles + "   <a href='" + val.productUrl + "'>Product's Link</a></p>";
-          html += "<p>Availability: " + val.availability + "    Ecological: " + val.ecological + "    Packaging: " + val.packaging + "</p></div>";
-          html += "</div>";
-          html += "<div  class='vinItem__price'><p>" + val.price + " kr </p>";
-          html += "</div>";
-          html += "</div>";
+        html += "<div class='vinItem'>";
+        html += "<div class='vinItem__photo'>";
+        html += "<img src='" + logo + "'>";
+        html += "<p class='vinItem__id'>" + val.id + "</p>";
+        html += "</div>";
+        html += "<div class='vinItem__info'>";
+        html += "<p class='vinItem__country'>" + val.country + " | " + val.region + "</p>";
+        html += "<p><h3>" + val.name + "</h3></p>";
+        html += "<p class='vinItem__category'><span class='cat-" + cat + "'>"+val.product+ "</span> | " + val.grapes + " | " + val.productionYear + "</p>";
+        html += "<p class='vinItem__rate'><span class='star-" + star +"'></span>Rate: " + Math.floor(val.rate*100)/100 + "</p>";
+        html += "<div class='vinItem__small'><p >Sold Bottles: " + val.soldBottles + "   <a href='" + val.productUrl + "'>Product's Link</a></p>";
+        html += "<p>Availability: " + val.availability + "    Ecological: " + val.ecological + "    Packaging: " + val.packaging + "</p></div>";
+        html += "</div>";
+        html += "<div  class='vinItem__price'><p>" + val.price + " kr </p>";
+        html += "</div>";
+        html += "</div>";
+      });
+      $vin.html(html);
 
-        });//end data.forEach
-        $vin.html(html);
-
-        //Array of all countries sorted a-z, filtered for duplicates
-        let countryArr = $.map(data, (el) => { return el.country })
-        .filter((elem, index, self) => { return index == self.indexOf(elem) })
-        .sort((a,b) => { return a.localeCompare(b)});
+      //Array of all countries sorted a-z, filtered for duplicates
+  //    let countryArr = $.map(data, (el) => { return el.country }).filter((elem, index, self) => { return index == self.indexOf(elem) }).sort((a,b) => { return a.localeCompare(b)});
 
       }, "json");
-  }//end getDefault
-
+  }
 
     //  $("#country").append('<option value=' + value[i] + '>' + countryArr[i] + '</option>');
 
-
-
-
-
 // on multiple select change
-  $pages.change(function() {
-    let selectedPages = [];
-
-    $("#pages :selected").each(() => {
-        selectedPages.push($(this).val());
+  $pages.change(function(){
+    var selectedPages = [];
+    $("#pages :selected").each(function(){
+      selectedPages.push($(this).val());
+      console.log(selectedPages);
     });
     vinURL = "http://vinguiden-webapp-develop.seals.schibsted.pl/api/product?ids="+selectedPages;
     getDefault();
     $('#country').val("default");
   });
-  // select change
-    $('#country').change(() => {
 
-      let selectedItem = $("#country :selected");
-      $.getJSON(vinURL, function(data){jsonData = data});
-        if(selectedItem.val() === "default"){
-          jsonDataFiltered = jsonData;
-          getDefault();
-        }else{
-          jsonDataFiltered = jsonData.filter(i => {
-            return i.country === selectedItem.val();
-          });
-          jsonData = jsonDataFiltered;
-          showJson();
-      }
-    }); //end country select change
+  // select change
+  $('#country').change(function(){
+    var selectedItem = $("#country :selected");
+    $.getJSON(vinURL, function(data){jsonData = data});
+    if(selectedItem.val() === "default"){
+      jsonDataFiltered = jsonData;
+      getDefault();
+    }else{
+      jsonDataFiltered = jsonData.filter(function(i){
+       return  i.country === selectedItem.val();
+      });
+      jsonData = jsonDataFiltered;
+      showJson();
+    }
+  });
 
 //***** Menu buttons **********//
   function sortPrice(){
      if(sortOrder === 'asc'){
-       jsonData = jsonDataFiltered.sort((a,b) => { return a.price - b.price});
+       jsonData = jsonDataFiltered.sort(function(a,b) {return a.price - b.price});
        sortOrder = 'desc';
      }else if(sortOrder === 'desc'){
-       jsonData = jsonDataFiltered.sort((a,b) => { return b.price - a.price});
+       jsonData = jsonDataFiltered.sort(function(a,b){return b.price - a.price});
        sortOrder = 'asc';
      }
   }
 
 function sortName(){
   if(sortOrder === 'asc'){
-    jsonData = jsonDataFiltered.sort((a,b) => { return a.name.localeCompare(b.name)});
+    jsonData = jsonDataFiltered.sort(function(a,b){ return a.name.localeCompare(b.name)});
     sortOrder = 'desc';
   }else if(sortOrder === 'desc'){
-    jsonData = jsonDataFiltered.sort((a,b) => { return b.name.localeCompare(a.name)});
+    jsonData = jsonDataFiltered.sort(function(a,b){ return b.name.localeCompare(a.name)});
     sortOrder = 'asc';
   }
 }
@@ -123,50 +114,41 @@ function showJson(){
 }
 
   function itemLayout(){
-    //setting placeholder if no img given
     jsonData[i].imageUrl !== null ? logo = jsonData[i].imageUrl : logo ='./assets/img/placeholder.png';
-    //number of stars
     jsonData[i].rate === null ? star = 'null': star = Math.floor(jsonData[i].rate);
-    //setting icon for wine type
     jsonData[i].product === "Rött vin" ? cat = 1 : jsonData[i].product === "Vitt vin" ? cat = 2 : jsonData[i].product === "Rosévin" ? cat = 3 : cat = 0;
 
-      html += "<div class='vinItem'>";
-      html += "<div class='vinItem__photo'>";
-      html += "<img src='" + logo + "'>";
-      html += "<p class='vinItem__id'>" + jsonData[i].id + "</p>";
-      html += "</div>";
-      html += "<div class='vinItem__info'>";
-      html += "<p class='vinItem__country'>" + jsonData[i].country + " | " + jsonData[i].region + "</p>";
-      html += "<p><h3>" + jsonData[i].name + "</h3></p>";
-      html += "<p class='vinItem__category'><span class='cat-" + cat + "'>"+jsonData[i].product+ "</span> | " + jsonData[i].grapes + " | " + jsonData[i].productionYear + "</p>";
-      html += "<p class='vinItem__rate'><span class='star-" + star +"'></span>Rate: " + Math.floor(jsonData[i].rate*100)/100 + "</p>";
-      html += "<div class='vinItem__small'><p >Sold Bottles: " + jsonData[i].soldBottles + "   <a href='" + jsonData[i].productUrl + "'>Product's Link</a></p>";
-      html += "<p>Availability: " + jsonData[i].availability + "    Ecological: " + jsonData[i].ecological + "    Packaging: " + jsonData[i].packaging + "</p></div>";
-      html += "</div>";
-      html += "<div  class='vinItem__price'><p>" + jsonData[i].price + " kr </p>";
-      html += "</div>";
-      html += "</div>";
-}
+    html += "<div class='vinItem'>";
+    html += "<div class='vinItem__photo'>";
+    html += "<img src='" + logo + "'>";
+    html += "<p class='vinItem__id'>" + jsonData[i].id + "</p>";
+    html += "</div>";
+    html += "<div class='vinItem__info'>";
+    html += "<p class='vinItem__country'>" + jsonData[i].country + " | " + jsonData[i].region + "</p>";
+    html += "<p><h3>" + jsonData[i].name + "</h3></p>";
+    html += "<p class='vinItem__category'><span class='cat-" + cat + "'>"+jsonData[i].product+ "</span> | " + jsonData[i].grapes + " | " + jsonData[i].productionYear + "</p>";
+    html += "<p class='vinItem__rate'><span class='star-" + star +"'></span>Rate: " + Math.floor(jsonData[i].rate*100)/100 + "</p>";
+    html += "<div class='vinItem__small'><p >Sold Bottles: " + jsonData[i].soldBottles + "   <a href='" + jsonData[i].productUrl + "'>Product's Link</a></p>";
+    html += "<p>Availability: " + jsonData[i].availability + "    Ecological: " + jsonData[i].ecological + "    Packaging: " + jsonData[i].packaging + "</p></div>";
+    html += "</div>";
+    html += "<div  class='vinItem__price'><p>" + jsonData[i].price + " kr </p>";
+    html += "</div>";
+    html += "</div>";
+  }
 
-
-
-
-
-  $('.sort__price').click(() => {
+  $('.sort__price').click(function(){
     $(this).find('i').toggleClass('fa-sort-numeric-desc fa-sort-numeric-asc');
      sortPrice();
      showJson();
   });
 
-  $('.sort__name').click(() => {
+  $('.sort__name').click(function(){
     $(this).find('i').toggleClass('fa-sort-alpha-desc fa-sort-alpha-asc');
     sortName();
     showJson();
   });
 
-  $(".styled-select").on({mouseenter:() => {
-    $pages.fadeIn();
-  }, mouseleave:() => {
-    $pages.fadeOut();
-  }});
-});//ready
+  $(".styled-select").on({
+    mouseenter:(function(){$pages.fadeIn()}), mouseleave:(function(){ $pages.fadeOut()})
+  });
+//});//ready
